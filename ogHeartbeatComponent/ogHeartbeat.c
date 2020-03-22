e9b9c7d8170d2a394c99442a8d663de8b564a427
mangoh <mangoh@mangoh.io>
2020-03-22 04:01:00 +0000

    new mangoh yellow support changes

--- ogHeartbeatComponent/ogHeartbeat.c
+++ ogHeartbeatComponent/ogHeartbeat.c
@@ -2,19 +2,19 @@
 #include "interfaces.h"
 //#include "le_limit_interface.h" cant get this working, switching to shell script
 //#include "le_appCtrl_common.h"
+//#include "le_limit_interface.h"
+//#include "le_appInfo_common.h"
 
-#define PUSH_TIMER_IN_MS (1000)
-//#define LED_BLINK_TIMER_IN_MS (250)
-//#define LED_BLINK_TIMER_IN_MS (250)
+#define BLINK_TIMER_IN_MS (600)
+#define APP_START_TIMER_IN_MS (1000)
+#define SHUTDOWN_TIMER_IN_MS (6000)
 
 static bool LedOn;
 static bool appsRunning;
 static bool shuttingDown;
-static le_timer_Ref_t PushTimerRef;
+static le_timer_Ref_t AppTimerRef;
+static le_timer_Ref_t ShutdownTimerRef;
 static le_timer_Ref_t BlinkTimerRef;
-int secondCount;
-int blinkCount;
-//static le_timer_Ref_t LEDBlinkTimerRef;
 
 
 static void BlinkTimer
@@ -24,50 +24,113 @@ static void BlinkTimer
 {
 		if(LedOn)
 		{
-			mangoh_led_Deactivate();
+			//mangoh_led_Deactivate();
 			LedOn = false;
 		}else
 		{
-			mangoh_led_Activate();
+			//mangoh_led_Activate();
 			LedOn = true;
 		}
 }
 //--------------------------------------------------------------------------------------------------
 /**
- * PushTimer is invoked every PUSH_TIMER_IN_MS (1000 right now) and will increment secondCount by one each time
+ * AppTimer is invoked every PUSH_TIMER_IN_MS (1000 right now) and will increment secondCount by one each time
  * initiate shutdown sequence when the timer "ticks" three times, i.e. the pushbutton has been held down for 3xPUSH_TIMER_IN_MS
  *  - make LED blink once shutdown is initiated
  */
 //--------------------------------------------------------------------------------------------------
-static void PushTimer
+static void AppTimer
 (
-    le_timer_Ref_t PushTimerRef
+    le_timer_Ref_t AppTimerRef
 )
 {
-	LE_INFO("PushTimer Invoked");
-	//count up by one each time the PushTimer is triggered
-	//if(mangoh_button_Read())
-	//{
-		secondCount++;
+	LE_INFO("AppTimer Invoked");
+	le_timer_Start(BlinkTimerRef);	//start blinking to notify user
+	le_timer_Stop(ShutdownTimerRef);//stop this from being invoked until after apps start/stop
+	le_timer_Stop(AppTimerRef); //stop from this coming in mutliple times
+	if (!appsRunning) //start apps
+	{
+		int systemResult;
+		time_t     now;
+		struct tm  ts;
+		char timestamp[80] = {0};
+		char systemCommand[300] = {0};
 		
-		if (secondCount == 1)
-		{
-			LE_INFO("Second Count 1");
-		}
-		if (secondCount == 3) 
-		{
-			LE_INFO("Second count 3");
-			//blink light and shutdown
-			le_timer_Stop(PushTimerRef);
-			shuttingDown = true;
-			//use this to set boot trigger is desired in future
-			//	LE_ASSERT_OK(le_ulpm_BootOnGpio(36, LE_ULPM_GPIO_HIGH));
+		time(&now);
+		
+		ts = *localtime(&now);
+		strftime(timestamp, sizeof(timestamp), "%Y-%m-%d-%H-%M-%S", &ts);
+		
+		sprintf(systemCommand, "echo %s > /mnt/userrw/sdcard/lastStartTime.txt", timestamp);
+		systemResult = system(systemCommand);
 
-			//  LE_ASSERT_OK(le_ulpm_ShutDown()); //devmode must be un-installed for this command to work!!
-			//shuttingDown = true;
-			//le_timer_Start(LEDBlinkTimerRef);
-			system("/sbin/sys_shutdown");
+		systemResult = system("/legato/systems/current/apps/ogHeartbeat/read-only/var/ogApps.sh start");
+		
+		//le_appCtrl_Start("gnssLogger");
+		
+		//Return value of -1 means that the fork() has failed (see man system).
+		if (0 == WEXITSTATUS(systemResult))
+		{
+			LE_INFO("Starting Apps Success");
+			appsRunning = true;
 		}
+		else
+		{
+			LE_ERROR("Error starting apps Failed: (%d)", systemResult);
+			le_timer_Stop(BlinkTimerRef);	
+			//mangoh_led_Deactivate();
+			LedOn = false;
+		}
+
+	}
+	else // stop apps
+	{
+		int systemResult;
+		le_timer_Stop(BlinkTimerRef);
+		
+
+		systemResult = system("/legato/systems/current/apps/ogHeartbeat/read-only/var/ogApps.sh stop");
+		// Return value of -1 means that the fork() has failed (see man system).
+		if (0 == WEXITSTATUS(systemResult))
+		{
+			LE_INFO("Stopping Apps Success");
+			appsRunning = false;
+			//mangoh_led_Activate();
+			LedOn = true;
+		}
+		else
+		{
+			LE_ERROR("Error stopping apps Failed: (%d)", systemResult);
+			//mangoh_led_Deactivate();
+			LedOn = false;
+		}	
+		
+	}
+	le_timer_Start(ShutdownTimerRef);
+	
+}
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * AppTimer is invoked every PUSH_TIMER_IN_MS (1000 right now) and will increment secondCount by one each time
+ * initiate shutdown sequence when the timer "ticks" three times, i.e. the pushbutton has been held down for 3xPUSH_TIMER_IN_MS
+ *  - make LED blink once shutdown is initiated
+ */
+//--------------------------------------------------------------------------------------------------
+static void ShutdownTimer
+(
+    le_timer_Ref_t AppTimerRef
+)
+{
+	LE_INFO("ShutdownTimer Invoked");
+	shuttingDown = true;
+	//use this to set boot trigger is desired in future
+	//	LE_ASSERT_OK(le_ulpm_BootOnGpio(36, LE_ULPM_GPIO_HIGH));
+
+	//  LE_ASSERT_OK(le_ulpm_ShutDown()); //devmode must be un-installed for this command to work!!
+
+	system("/sbin/sys_shutdown");
+
 	
 }
 
@@ -88,68 +151,15 @@ static void PushButtonHandler
 		if (state) //button pushed
 		{
 			LE_INFO("Starting Push Button Timer");
-			le_timer_Start(PushTimerRef);
+			le_timer_Start(AppTimerRef);
+			le_timer_Start(ShutdownTimerRef);
 		}
 		else //button released
 		{
 			LE_INFO("Stopping push button timer");
-			le_timer_Stop(PushTimerRef);
-			if (secondCount >= 1) //start or stop apps
-			{
-				if (!appsRunning) //start apps
-				{
-					int systemResult;
-					time_t     now;
-					struct tm  ts;
-					char timestamp[80] = {0};
-					char systemCommand[300] = {0};
-					
-					time(&now);
-					
-					ts = *localtime(&now);
-					strftime(timestamp, sizeof(timestamp), "%Y-%m-%d-%H-%M-%S", &ts);
-					
-					sprintf(systemCommand, "echo %s > /mnt/userrw/sdcard/lastStartTime.txt", timestamp);
-					systemResult = system(systemCommand);
+			le_timer_Stop(AppTimerRef);
+			le_timer_Stop(ShutdownTimerRef);
 
-					systemResult = system("/legato/systems/current/apps/ogHeartbeat/read-only/var/ogApps.sh start");
-					
-					// Return value of -1 means that the fork() has failed (see man system).
-					if (0 == WEXITSTATUS(systemResult))
-					{
-						LE_INFO("Starting Apps Success");
-						appsRunning = true;
-						le_timer_Start(BlinkTimerRef);	
-					}
-					else
-					{
-						LE_ERROR("Error starting apps Failed: (%d)", systemResult);
-					}
-
-				}
-				else // stop apps
-				{
-					int systemResult;
-
-					systemResult = system("/legato/systems/current/apps/ogHeartbeat/read-only/var/ogApps.sh stop");
-					// Return value of -1 means that the fork() has failed (see man system).
-					if (0 == WEXITSTATUS(systemResult))
-					{
-						LE_INFO("Stopping Apps Success");
-						appsRunning = false;
-						le_timer_Stop(BlinkTimerRef);
-						mangoh_led_Deactivate();
-						LedOn = false;
-			
-					}
-					else
-					{
-						LE_ERROR("Error stopping apps Failed: (%d)", systemResult);
-					}			
-				}
-			}
-			//reset second count to 0 when button released
-			secondCount = 0;
 		}
     }
 }
@@ -163,10 +173,10 @@ static void PushButtonHandler
 static void ConfigureGpios(void)
 {
     // Set LED GPIO to output and initially turn the LED ON
-    LE_FATAL_IF(
+   /* LE_FATAL_IF(
         mangoh_led_SetPushPullOutput(MANGOH_LED_ACTIVE_HIGH, true) != LE_OK,
         "Couldn't configure LED GPIO as a push pull output");
-    LedOn = true;
+    LedOn = true;*/
 
     // Set the push-button GPIO as input
     LE_FATAL_IF(
@@ -174,26 +184,32 @@ static void ConfigureGpios(void)
         "Couldn't configure push button as input");
     mangoh_button_AddChangeEventHandler(MANGOH_BUTTON_EDGE_BOTH, PushButtonHandler, NULL, 0);
 }
+
+
 //remember to make sure that AT (AT+WIOCFG) command has been set for gpio and that gpio expander service is installed and running.
 COMPONENT_INIT
 {
 	appsRunning = false;
 	shuttingDown = false;
-	secondCount = 0;
-    PushTimerRef = le_timer_Create("Push Button Timer");
-    le_timer_SetMsInterval(PushTimerRef, PUSH_TIMER_IN_MS);
-    le_timer_SetRepeat(PushTimerRef, 0);
-    le_timer_SetHandler(PushTimerRef, PushTimer);
+    AppTimerRef = le_timer_Create("App Timer");
+    le_timer_SetMsInterval(AppTimerRef, APP_START_TIMER_IN_MS);
+    le_timer_SetRepeat(AppTimerRef, 0);
+    le_timer_SetHandler(AppTimerRef, AppTimer);
+    
+    ShutdownTimerRef = le_timer_Create("Shutdown Timer");
+    le_timer_SetMsInterval(ShutdownTimerRef, SHUTDOWN_TIMER_IN_MS);
+    le_timer_SetRepeat(ShutdownTimerRef, 0);
+    le_timer_SetHandler(ShutdownTimerRef, ShutdownTimer);
     
     BlinkTimerRef = le_timer_Create("Blink Timer");
-    le_timer_SetMsInterval(BlinkTimerRef, 600);
+    le_timer_SetMsInterval(BlinkTimerRef, BLINK_TIMER_IN_MS);
     le_timer_SetRepeat(BlinkTimerRef, 0);
     le_timer_SetHandler(BlinkTimerRef, BlinkTimer);
     
    
     ConfigureGpios();
     
-    mangoh_led_Activate();
+   // mangoh_led_Activate(); //turn on LED to show user ogHeartBeat is on. 
 	LedOn = true;
     
 
